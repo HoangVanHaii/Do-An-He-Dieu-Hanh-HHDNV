e01cc7ed62399abfc4c28deaf6ba39307e7c0446
HoangVanHai <hoanggvanhaii0906@gmail.com>
2025-05-21 19:39:25 +0700

    Chỉnh giao diện form main

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -242,9 +242,9 @@ namespace CPUSchedulerProject {
             this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.panel2.Controls.Add(this.label10);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel2.Location = new System.Drawing.Point(0, 401);
+            this.panel2.Location = new System.Drawing.Point(0, 411);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1090, 164);
+            this.panel2.Size = new System.Drawing.Size(1090, 184);
             this.panel2.TabIndex = 1;
             // 
             // label10
@@ -252,7 +252,7 @@ namespace CPUSchedulerProject {
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label10.ForeColor = System.Drawing.Color.Red;
-            this.label10.Location = new System.Drawing.Point(44, 24);
+            this.label10.Location = new System.Drawing.Point(35, 11);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(156, 24);
             this.label10.TabIndex = 0;
@@ -267,7 +267,7 @@ namespace CPUSchedulerProject {
             this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel3.Location = new System.Drawing.Point(0, 255);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(1090, 146);
+            this.panel3.Size = new System.Drawing.Size(1090, 156);
             this.panel3.TabIndex = 2;
             // 
             // panel8
@@ -281,7 +281,7 @@ namespace CPUSchedulerProject {
             this.panel8.Dock = System.Windows.Forms.DockStyle.Right;
             this.panel8.Location = new System.Drawing.Point(788, 0);
             this.panel8.Name = "panel8";
-            this.panel8.Size = new System.Drawing.Size(302, 146);
+            this.panel8.Size = new System.Drawing.Size(302, 156);
             this.panel8.TabIndex = 1;
             this.panel8.Paint += new System.Windows.Forms.PaintEventHandler(this.panel6_Paint);
             // 
@@ -326,7 +326,7 @@ namespace CPUSchedulerProject {
             this.panel7.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel7.Location = new System.Drawing.Point(368, 0);
             this.panel7.Name = "panel7";
-            this.panel7.Size = new System.Drawing.Size(722, 146);
+            this.panel7.Size = new System.Drawing.Size(722, 156);
             this.panel7.TabIndex = 1;
             this.panel7.Paint += new System.Windows.Forms.PaintEventHandler(this.panel6_Paint);
             // 
@@ -355,7 +355,7 @@ namespace CPUSchedulerProject {
             this.panel6.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel6.Location = new System.Drawing.Point(0, 0);
             this.panel6.Name = "panel6";
-            this.panel6.Size = new System.Drawing.Size(368, 146);
+            this.panel6.Size = new System.Drawing.Size(368, 156);
             this.panel6.TabIndex = 1;
             this.panel6.Paint += new System.Windows.Forms.PaintEventHandler(this.panel6_Paint);
             // 
@@ -482,7 +482,7 @@ namespace CPUSchedulerProject {
             // 
             // MainForm
             // 
-            this.ClientSize = new System.Drawing.Size(1090, 565);
+            this.ClientSize = new System.Drawing.Size(1090, 595);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
